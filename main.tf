4941a274c484832d233e4368d88ef9f80d6dc761
yedukondalu-ceq <89455823+Yedukondalu-ceq@users.noreply.github.com>
2024-01-09 20:34:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,8 +38,16 @@ resource "random_pet" "sg" {}
 #   owners = ["099720109477"] # Canonical
 # }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "terraformcloud-tfci2"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
 # resource "aws_instance" "web" {
-#   ami                    = data.aws_ami.ubuntu.id
+#   ami                    = "ami-0c7217cdde317cfec"
 #   instance_type          = "t2.large"
 #   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
@@ -55,24 +63,24 @@ resource "random_pet" "sg" {}
 #     Name = "tf-example"
 #     Demo = "f&p"
 #   }
-# }
+#}
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#resource "aws_security_group" "web-sg" {
+#  name = "${random_pet.sg.id}-sg"
+#  ingress {
+#    from_port   = 8080
+#    to_port     = 8080
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
 
 # output "web-address" {
 #   value = "${aws_instance.web.public_dns}:8080"
