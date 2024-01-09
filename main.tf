62b6dc35242242818b9c4c9bf066c0f5ec27825c
yedukondalu-ceq <89455823+Yedukondalu-ceq@users.noreply.github.com>
2024-01-09 20:06:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-2"
-  access_key = var.AWS_ACCESS_KEY_ID   
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+#  access_key = var.AWS_ACCESS_KEY_ID   
+#  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "random_pet" "sg" {}
